47988ce344bee6156cefb43a785648bb82848ede
eremiev <emil.eremiev@gmail.com>
2017-03-19 13:49:36 +0200

    Start an Activity with an Intent

--- GroceryList/MainActivity.cs
+++ GroceryList/MainActivity.cs
@@ -7,43 +7,47 @@ using System.Collections.Generic;
 
 namespace GroceryList
 {
-	[Activity(Label = "Grocery List", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		public static List<Item> Items = new List<Item>();
+    [Activity(Label = "Grocery List", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public static List<Item> Items = new List<Item>();
 
-		protected override void OnCreate(Bundle bundle)
-		{
-			Items.Add(new Item("Milk",     2));
-			Items.Add(new Item("Crackers", 1));
-			Items.Add(new Item("Apples",   5));
+        protected override void OnCreate(Bundle bundle)
+        {
+            Items.Add(new Item("Milk", 2));
+            Items.Add(new Item("Crackers", 1));
+            Items.Add(new Item("Apples", 5));
 
-			base.OnCreate(bundle);
-			SetContentView(Resource.Layout.Main);
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
 
-			FindViewById<Button>(Resource.Id.itemsButton  ).Click += OnItemsClick;
-			FindViewById<Button>(Resource.Id.addItemButton).Click += OnAddItemClick;
-			FindViewById<Button>(Resource.Id.aboutButton  ).Click += OnAboutClick;
-		}
+            FindViewById<Button>(Resource.Id.itemsButton).Click += OnItemsClick;
+            FindViewById<Button>(Resource.Id.addItemButton).Click += OnAddItemClick;
+            FindViewById<Button>(Resource.Id.aboutButton).Click += OnAboutClick;
+        }
 
-		void OnItemsClick(object sender, EventArgs e)
-		{
-			// TODO
-		}
+        void OnItemsClick(object sender, EventArgs e)
+        {
+            // TODO
+            var intent = new Intent(this, typeof(ItemsActivity));
+            StartActivity(intent);
+        }
 
-		void OnAddItemClick(object sender, EventArgs e)
-		{
-			// TODO
-		}
+        void OnAddItemClick(object sender, EventArgs e)
+        {
+            // TODO
+            var intent = new Intent(this, typeof(AddItemActivity));
+            StartActivity(intent);
+        }
 
-		void OnAboutClick(object sender, EventArgs e)
-		{
-			// TODO
-		}
+        void OnAboutClick(object sender, EventArgs e)
+        {
+            // TODO
+        }
 
-		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-		{
-			// TODO
-		}
-	}
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            // TODO
+        }
+    }
 }
