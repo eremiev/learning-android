8abd737b21ed0504de9c556e5f28b7f4d9648b98
eremiev <emil.eremiev@gmail.com>
2017-03-19 14:37:01 +0200

    Play with StartActivityForResult

--- GroceryList/AddItemActivity.cs
+++ GroceryList/AddItemActivity.cs
@@ -6,30 +6,36 @@ using Android.Widget;
 
 namespace GroceryList
 {
-	[Activity(Label = "Add Item")]			
-	public class AddItemActivity : Activity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			SetContentView(Resource.Layout.AddItem);
+    [Activity(Label = "Add Item")]
+    public class AddItemActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.AddItem);
 
-			FindViewById<Button>(Resource.Id.saveButton  ).Click += OnSaveClick;
-			FindViewById<Button>(Resource.Id.cancelButton).Click += OnCancelClick;
-		}
+            FindViewById<Button>(Resource.Id.saveButton).Click += OnSaveClick;
+            FindViewById<Button>(Resource.Id.cancelButton).Click += OnCancelClick;
+        }
 
-		void OnSaveClick(object sender, EventArgs e)
-		{
-			string name  = FindViewById<EditText>(Resource.Id.nameInput).Text;
-			int    count = int.Parse(FindViewById<EditText>(Resource.Id.countInput).Text);
+        void OnSaveClick(object sender, EventArgs e)
+        {
+            string name = FindViewById<EditText>(Resource.Id.nameInput).Text;
+            int count = int.Parse(FindViewById<EditText>(Resource.Id.countInput).Text);
 
-			// TODO
-		}
+            // TODO
+            var intent = new Intent();
 
-		void OnCancelClick(object sender, EventArgs e)
-		{
+            intent.PutExtra("ItemName", name);
+            intent.PutExtra("ItemCount", count);
+            SetResult(Result.Ok, intent);
+            Finish();
+        }
+
+        void OnCancelClick(object sender, EventArgs e)
+        {
             // TODO
             Finish();
         }
-	}
+    }
 }
